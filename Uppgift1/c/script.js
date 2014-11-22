447ed976d11567c5aa6b9b5a3d7f19a528481be1
mw222rs <mw222rs@student.lnu.se>
2014-11-22 09:24:07 +0000

    fixing small stuff

---
+++ Uppgift1/c/script.js
@@ -0,0 +1,36 @@
+"use strict";
+
+window.onload = function(){
+
+	var aCounter = function(str){
+	    var capital = 0;
+	    var lowercase = 0;
+			
+		var arrayed = str.split("");
+		
+		var mapArrayed = arrayed.map(function(item, index, array){
+			
+			if (item === "A") {
+				capital += 1;
+			}
+			if (item === "a") {
+				lowercase += 1;
+			}
+			
+			// return (item === item.toUpperCase()) ? item.toLowerCase() : item.toUpperCase();
+		});
+		
+		return "Antal A: " + capital + ", antal a: " + lowercase;
+	
+	};
+	
+	var p = document.querySelector("#value");
+	var input = document.querySelector("#string");
+	var submit = document.querySelector("#send");
+
+	submit.addEventListener("click", function(e){
+		e.preventDefault(); 
+			var answer = aCounter(input.value); 
+			p.innerHTML = answer;
+	});
+};
